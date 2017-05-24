95f590561f0bcb34b84affb7b0f952b2d4dd2dc5
ZhangCui <zczhangcui@foxmail.com>
2017-05-24 16:29:28 +0800

    update

--- photo/app.js
+++ photo/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
+
 
 var app = express();
 
@@ -22,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', photos);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
