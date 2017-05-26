64e36f62d4f8a8c98a7de7d866ebe7a96c3fb610
ZhangCui <zczhangcui@foxmail.com>
2017-05-26 18:08:55 +0800

    update

--- photo/app.js
+++ photo/app.js
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// app.set('photos', path.join(__dirname, 'public/photos'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', photos);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- photo/routes/photos.js
+++ photo/routes/photos.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Photo = require('../models/Photo');
+var path = require('path');
+var multer = require('multer'); 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, '../public/photos'));
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
+});
+
+var upload = multer({ storage: storage });
+
 
 var photos = [];
 photos.push({
@@ -12,11 +26,40 @@ photos.push({
 	path: 'http://nodejs.org/images/ryan-speaker.jpg'
 });
 
-router.get('/', function(req, res) {
-	res.render('photos', { 
-		title: 'Photos',
-		photos: photos
+router.get('/', function(req, res, next) {
+	Photo.find({}, function(err, photos) {
+		if (err) {
+			return next(err);
+		}
+		res.render('photos', { 
+			title: 'Photos',
+			photos: photos
+		});
 	});
+	
+});
+
+router.get('/upload', function(req, res) {
+	res.render('photos/upload', {
+		title: 'Photo upload'
+	});
+});
+
+router.post('/upload', upload.any(), function(req, res, next) {
+	var name = req.body.photo.name;
+	var _path = path.join('/photos', req.files[0].originalname);
+
+	var photo = new Photo({
+		name: name,
+		path: _path
+	});
+	photo.save(function(err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/');
+	});
+
 });
 
 module.exports = router;
